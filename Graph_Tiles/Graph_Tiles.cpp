1a8a86548b55f1db352d7dbce69ad370bbf29a63
Kuchinawa <gert_meijer89@hotmail.com>
2025-05-14 07:21:28 +0200

    Add project files.

---
+++ Graph_Tiles/Graph_Tiles.cpp
@@ -0,0 +1,74 @@
+﻿// Graph_Tiles.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+#include <stdio.h>
+#include <fstream>
+#include <iostream>
+#include <vector>
+
+#include "Tile.h"
+
+using Grid = std::vector<std::vector<Tile>>;
+
+int print_grid(Grid& grid)
+{
+    
+
+    if (!file)
+    {
+        std::cerr << "Could not open file!\n";
+        return 1;
+    }
+
+
+    for (const auto& row : grid)
+    {
+        for (const auto& cell : row)
+        {
+            file << cell.tile_to_string();
+        }
+
+        file << "\n";
+    }
+
+    file.close();
+}
+
+Grid generate_grid(const std::vector<int>& tiles)
+{
+    Grid grid;
+
+    int grid_x = 2;
+    int grid_y = 2;
+
+    for (size_t j = 0; j < grid_y; j++)
+    {
+        std::vector<Tile> row;
+
+        for (size_t i = 0; i < grid_x; i++)
+        {
+            row.push_back(Tile(tiles.at(j * grid_x + i)));
+        }
+
+        grid.push_back(row);
+    }
+
+    return grid;
+}
+
+int main()
+{
+    std::ofstream file("grid_output.txt");
+    int tile_type = 16;
+
+    for (int a = 0; a < 16; ++a)
+        for (int b = 0; b < 16; ++b)
+            for (int c = 0; c < 16; ++c)
+                for (int d = 0; d < 16; ++d)
+                {
+                    std::vector<int> tiles{ a,b,c,d };
+                    Grid g = generate_grid(tiles);
+                    print_grid(g);
+                }
+
+
+}
---
+++ Graph_Tiles/Tile.h
@@ -0,0 +1,48 @@
+﻿#pragma once
+
+#include <string>
+#include <array>
+#include <unordered_map>
+#include <map>
+
+enum class Orientation
+{
+    Up = 0,
+    Left = 1,
+    Down = 2,
+    Right = 3
+};
+
+
+class Tile
+{
+public:
+
+    static const std::unordered_map<int, const char*> tile_strings;
+
+    explicit Tile(int exits) : exits(exits)
+    {
+
+    }
+
+    Orientation orientation;
+
+    uint8_t exits;
+
+    uint8_t rotate_tile(int times_clockwise_90)
+    {
+        times_clockwise_90 %= 4;
+        for (int i = 0; i < times_clockwise_90; ++i) {
+            // Rotate bits right with wrap-around
+            exits = ((exits >> 1) | ((exits & 1) << 3)) & 0b1111;
+        }
+        return exits;
+    }
+
+    std::string tile_to_string() const
+    {
+        auto it = tile_strings.find(exits);
+        return it != tile_strings.end() ? it->second : "?";
+    }
+};
+
