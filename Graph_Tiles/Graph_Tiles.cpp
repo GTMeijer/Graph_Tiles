efcc8d6e61090fcb3da07ce14c5670020407586e
Kuchinawa <gert_meijer89@hotmail.com>
2025-05-14 10:43:31 +0200

    Output

--- Graph_Tiles/Graph_Tiles.cpp
+++ Graph_Tiles/Graph_Tiles.cpp
@@ -33,28 +33,6 @@ int print_grid(Grid& grid, std::ofstream& output)
 
 }
 
-Grid generate_grid(const std::vector<int>& tiles)
-{
-    Grid grid;
-
-    int grid_x = 2;
-    int grid_y = 2;
-
-    for (size_t j = 0; j < grid_y; j++)
-    {
-        std::vector<Tile> row;
-
-        for (size_t i = 0; i < grid_x; i++)
-        {
-            row.push_back(Tile(tiles.at(j * grid_x + i)));
-        }
-
-        grid.push_back(row);
-    }
-
-    return grid;
-}
-
 bool validate_grid(const Grid& grid)
 {
     const int height = grid.size();
@@ -190,13 +168,27 @@ std::vector<std::vector<Tile>> init_grid(int width, int height)
     return std::vector<std::vector<Tile>>(height, std::vector<Tile>(width, Tile()));
 }
 
+// https://stackoverflow.com/a/101613/3738557
+unsigned long long int ipow(int base, int exp)
+{
+    unsigned long long int result = 1;
+    unsigned long long int lbase = base;
+    for (;;)
+    {
+        if (exp & 1)
+            result *= lbase;
+        exp >>= 1;
+        if (!exp)
+            break;
+        lbase *= lbase;
+    }
+
+    return result;
+}
+
 int main()
 {
-
-
-
     std::ofstream file("C:\\dev\\grid_output.txt");
-    int tile_types = 16;
 
     int valid = 0;
 
@@ -207,29 +199,14 @@ int main()
 
     backtrack(0, dimension_x, dimension_y, grid, valid, file);
 
-
-    std::cout << "Number of valid configurations: " << valid << "/" <<  << std::endl;
-
-    //for (int a = 0; a < 16; ++a)
-    //{
-    //    std::cout << a << std::endl;
-    //    for (int b = 0; b < 16; ++b)
-    //    {
-    //        for (int c = 0; c < 16; ++c)
-    //        {
-    //            for (int d = 0; d < 16; ++d)
-    //            {
-    //                std::vector<int> tiles{ a,b,c,d };
-    //                Grid g = generate_grid(tiles);
-    //                if (validate_grid(g))
-    //                {
-    //                    print_grid(g, file);
-    //                    //file.flush();
-    //                }
-    //            }
-    //        }
-    //    }
-    //}
-
+    if (valid >= 0)
+    {
+        //std::cout << "Number of valid configurations: " << valid << "/" << ipow(tile_types.size(), dimension_x * dimension_y) << std::endl;
+        std::cout << "Number of valid configurations: " << valid << std::endl;
+    }
+    else
+    {
+        std::cout << "No solutions found." << std::endl;
+    }
     file.close();
 }
